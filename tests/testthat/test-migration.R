997e5c0b381d9d2562d82aba2db8476e7caf03a2
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-09-24 17:48:08 +0100

    Fix migration tests to be incremental

--- tests/testthat/test-migration.R
+++ tests/testthat/test-migration.R
@@ -1,7 +1,8 @@
 test_that("Do nothing while migrating up-to-date sources", {
   path <- suppressMessages(orderly_example())
   info <- helper_add_git(path)
-  res <- evaluate_promise(orderly_migrate_source(path, from = "0"))
+  res <- evaluate_promise(
+    orderly_migrate_source(path, from = "0", to = "1.99.82"))
   expect_length(res$messages, 4)
   expect_match(res$messages[[1]], "Migrating from 1.99.0 to 1.99.82")
   expect_match(res$messages[[2]], "Checking \\d+ files in")
@@ -143,7 +144,7 @@ test_that("can migrate old sources", {
   info <- helper_add_git(path)
 
   res <- evaluate_promise(
-    orderly_migrate_source(path, from = "0", dry_run = TRUE))
+    orderly_migrate_source(path, to = "1.99.82", dry_run = TRUE))
   expect_length(res$messages, 5)
   expect_match(res$messages[[1]], "Migrating from 1.99.0 to 1.99.82")
   expect_match(res$messages[[2]], "Checking \\d+ files in")
@@ -155,7 +156,7 @@ test_that("can migrate old sources", {
   expect_equal(nrow(gert::git_status(repo = path)), 0)
 
   res <- evaluate_promise(
-    orderly_migrate_source(path))
+    orderly_migrate_source(path, to = "1.99.82"))
   expect_length(res$messages, 6)
   expect_match(res$messages[[1]], "Migrating from 1.99.0 to 1.99.82")
   expect_match(res$messages[[2]], "Checking \\d+ files in")
@@ -198,7 +199,7 @@ test_that("can migrate orderly.R files", {
   expect_length(dir(path, "orderly\\.R$", recursive = TRUE), length(nms))
 
   res <- evaluate_promise(
-    orderly_migrate_source(path, dry_run = TRUE))
+    orderly_migrate_source(path, to = "1.99.88", dry_run = TRUE))
   expect_length(res$messages, length(nms) + 3)
   n <- length(res$messages)
   expect_match(res$messages[[1]], "Migrating from 1.99.82 to 1.99.88")
@@ -212,7 +213,7 @@ test_that("can migrate orderly.R files", {
   expect_equal(nrow(gert::git_status(repo = path)), 0)
 
   res <- evaluate_promise(
-    orderly_migrate_source(path))
+    orderly_migrate_source(path, to = "1.99.88"))
   expect_length(res$messages, length(nms) + 4)
   expect_true(res$result)
 
